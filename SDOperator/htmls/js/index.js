ca41ad00cd0368009fa54d2b7559ac91fab47f9c
杨一凡 <340019109@qq.com>
2019-01-11 09:18:50 +0800

    【沙盒Demo】Step1：最简Html页面

---
+++ SDOperator/htmls/js/index.js
@@ -0,0 +1,18 @@
+
+function onSdAdd() {
+    console.log("Sandbox Add");
+}
+
+function onSdRemove() {
+    console.log("Sandbox Remove");
+}
+
+function onSdSelect() {
+    console.log("Sandbox Select");
+}
+
+function onSdSelectAll() {
+    console.log("Sandbox Select All");
+}
+
+
