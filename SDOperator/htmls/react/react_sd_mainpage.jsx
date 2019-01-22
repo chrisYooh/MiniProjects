127abbbd2a6d20f586379f0e13f9658d8e2cdbfd
杨一凡 <340019109@qq.com>
2019-01-22 11:11:20 +0800

    【沙盒Html】Step5：将所有类集中到一个文件（不理想，暂未理清多文件通信办法）

---
+++ SDOperator/htmls/react/react_sd_mainpage.jsx
@@ -0,0 +1,137 @@
+/*****************************************************  Sandbox Info  *****************************************************/
+class SdInfoItem extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {itemValue: " "};
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({itemValue: event.target.itemValue});
+    }
+
+    render() {
+        return (
+            <div className="sdinfo_item">
+                <label className="sditem_title">{this.props.itemTitle}</label>
+                <input className="sditem_value" type="text" value={this.state.itemValue} onChange={this.handleChange} />
+            </div>
+        )
+    }
+};
+
+class SandboxInfo extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {sdOpName: "查找沙盒"};
+    }
+
+    render() {
+        return (
+            <div className="sdinfo_board chris_coltopflex">
+                <p1 className="sdinfo_title">沙盒信息</p1>
+                <p2 className="sdinfo_opinfo">最近操作: {this.state.sdOpName}</p2>
+                <SdInfoItem itemTitle="ID" />
+                <SdInfoItem itemTitle="系统" />
+                <SdInfoItem itemTitle="IP" />
+                <SdInfoItem itemTitle="端口" />
+                <SdInfoItem itemTitle="其他配置" />
+            </div>
+        );
+    }
+}
+
+/*****************************************************  Sandbox Operation  *****************************************************/
+
+class SdOpButton extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    handleClick() {
+
+    }
+
+    render() {
+        return (
+            <button className="sdop_button">{this.props.title}</button>
+        )
+    }
+}
+
+class SandboxOp extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {isToggleOn: true};
+
+        // 这边绑定是必要的，这样 `this` 才能在回调函数中使用
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+    }
+
+    render() {
+        return (
+            <div className="sdop_board chris_coltopflex">
+                <p1 className="sdop_title">操作</p1>
+                <SdOpButton title="添加沙盒" />
+                <SdOpButton title="删除沙盒" />
+                <SdOpButton title="查找一个沙盒" />
+                <SdOpButton title="删除所有沙盒" />
+                <SdOpButton title="查找所有沙盒" />
+                {/*<button id="sd_create" onClick="onSdCreate()">添加沙盒</button>*/}
+                {/*<button id="sd_remove" onClick="onSdRemove()">删除沙盒</button>*/}
+                {/*<button id="sd_select" onClick="onSdSelect()">查找一个沙盒</button>*/}
+                {/*<button id="sd_remove_all" onClick="onSdRemoveAll()">删除所有沙盒</button>*/}
+                {/*<button id="sd_select_all" onClick="onSdSelectAll()">查找所有沙盒</button>*/}
+                {/*<br/>*/}
+                {/*<label id="sd_testLabel">*/}
+                {/*暂无数据*/}
+                {/*</label>*/}
+            </div>
+        );
+    }
+}
+
+/*****************************************************  Mainpage  *****************************************************/
+
+class SdMainpage extends React.Component {
+
+    constructor (props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div class="mainpage_baseboard flex-col">
+
+                {/* 标题 */}
+                <div class="mainpage_titleLabel chris_rowflex">
+                    沙盒构建演示Demo - 主页
+                </div>
+
+                {/* 内容 */}
+                <div id="mainpage_sd_board" class="mainpage_sd_board chris_rowflex">
+                    {/* 沙盒信息 */}
+                    <SandboxInfo />
+                    {/* 沙盒操作 */}
+                    <SandboxOp class="mainpage_sd_op"/>
+                </div>
+
+            </div>
+        )
+    }
+};
+
+/*****************************************************  Render  *****************************************************/
+
+ReactDOM.render(
+    <SdMainpage />,
+    document.getElementById('mainpage')
+);
--- SDOperator/htmls/react/testreact.js
+++
@@ -1,4 +0,0 @@
-ReactDOM.render(
-    <h1 class="mainpage_test">Hello, world!</h1>,
-    document.getElementById('example')
-);
