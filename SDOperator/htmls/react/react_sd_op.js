2594410876aa577847126ebf8bf990ec1d5e3a37
杨一凡 <340019109@qq.com>
2019-01-22 09:13:00 +0800

    【沙盒Html】Step4：基本完成沙盒操作页面的布局

--- SDOperator/htmls/react/react_sd_op.js
+++ SDOperator/htmls/react/react_sd_op.js
@@ -1,4 +1,22 @@
-class Toggle extends React.Component {
+class SdOpButton extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    handleClick() {
+
+    }
+
+    render() {
+        return (
+            <button className="sdop_button">{this.props.title}</button>
+        )
+    }
+}
+
+class SandboxOp extends React.Component {
+
     constructor(props) {
         super(props);
         this.state = {isToggleOn: true};
@@ -8,29 +26,32 @@ class Toggle extends React.Component {
     }
 
     handleClick() {
-        this.setState(prevState => ({
-            isToggleOn: !prevState.isToggleOn
-        }));
     }
 
     render() {
         return (
             <div className="chris_colflex">
-                <button id="sd_create" onClick="onSdCreate()">添加沙盒</button>
-                <button id="sd_remove" onClick="onSdRemove()">删除沙盒</button>
-                <button id="sd_select" onClick="onSdSelect()">查找一个沙盒</button>
-                <button id="sd_remove_all" onClick="onSdRemoveAll()">删除所有沙盒</button>
-                <button id="sd_select_all" onClick="onSdSelectAll()">查找所有沙盒</button>
-                <br/>
-                <label id="sd_testLabel">
-                    暂无数据
-                </label>
+                <p1 className="sdop_title">操作</p1>
+                <SdOpButton title="添加沙盒" />
+                <SdOpButton title="删除沙盒" />
+                <SdOpButton title="查找一个沙盒" />
+                <SdOpButton title="删除所有沙盒" />
+                <SdOpButton title="查找所有沙盒" />
+                {/*<button id="sd_create" onClick="onSdCreate()">添加沙盒</button>*/}
+                {/*<button id="sd_remove" onClick="onSdRemove()">删除沙盒</button>*/}
+                {/*<button id="sd_select" onClick="onSdSelect()">查找一个沙盒</button>*/}
+                {/*<button id="sd_remove_all" onClick="onSdRemoveAll()">删除所有沙盒</button>*/}
+                {/*<button id="sd_select_all" onClick="onSdSelectAll()">查找所有沙盒</button>*/}
+                {/*<br/>*/}
+                {/*<label id="sd_testLabel">*/}
+                    {/*暂无数据*/}
+                {/*</label>*/}
             </div>
         );
     }
 }
 
 ReactDOM.render(
-    <Toggle />,
+    <SandboxOp />,
     document.getElementById('mainpage_sd_op')
 );
